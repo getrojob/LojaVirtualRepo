e1e37daf735d15ca0ccc3a4c59b0ceb997071119
getrojob <getrojob@gmai.com>
2022-05-08 19:18:29 -0300

    add CategoryRepository e ProductRepository

---
+++ VShop.ProductApi/Models/Repositories/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+namespace VShop.ProductApi.Models.Repositories
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetAll();
+        Task<IEnumerable<Category>> GetCategoriesProducts();
+        Task<Category> GetById(int id);
+        Task<Category> Create(Category category);
+        Task<Category> Update(Category category);
+        Task<Category> Delete(int id);
+    }
+}
---
+++ VShop.ProductApi/Models/Repositories/IProductRepository.cs
@@ -0,0 +1,11 @@
+namespace VShop.ProductApi.Models.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetAll();
+        Task<Product> GetById(int id);
+        Task<Product> Create(Product product);
+        Task<Product> Update(Product product);
+        Task<Product> Delete(int id);
+    }
+}
