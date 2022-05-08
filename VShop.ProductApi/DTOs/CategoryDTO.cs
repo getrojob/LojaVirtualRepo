e672040cffeadfb47467317068c57fbd3d41a2f9
getrojob <getrojob@gmai.com>
2022-05-08 18:45:53 -0300

    add annotation

--- VShop.ProductApi/DTOs/CategoryDTO.cs
+++ VShop.ProductApi/DTOs/CategoryDTO.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using VShop.ProductApi.Models;
 
 namespace VShop.ProductApi.DTOs
@@ -5,6 +6,10 @@ namespace VShop.ProductApi.DTOs
     public class CategoryDTO
     {
         public int CategoryId { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [MinLength(3, ErrorMessage = "The field {0} must have a minimum of {1} characters")]
+        [MaxLength(100, ErrorMessage = "The field {0} must have a maximum of {1} characters")]
         public string? Name { get; set; }
         public ICollection<Product>? Products { get; set; }
     }
--- VShop.ProductApi/DTOs/ProductDTO.cs
+++ VShop.ProductApi/DTOs/ProductDTO.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using VShop.ProductApi.Models;
 
 namespace VShop.ProductApi.DTOs
@@ -5,9 +6,22 @@ namespace VShop.ProductApi.DTOs
     public class ProductDTO
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [MinLength(3, ErrorMessage = "The field {0} must have a minimum of {1} characters")]
+        [MaxLength(100, ErrorMessage = "The field {0} must have a maximum of {1} characters")]
         public string? Name { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
         public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [MinLength(5, ErrorMessage = "The field {0} must have a minimum of {1} characters")]
+        [MaxLength(200, ErrorMessage = "The field {0} must have a maximum of {1} characters")]
         public string? Description { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [Range(1, 9999, ErrorMessage = "The field {0} must be a number greater than {1}")]
         public long Stock { get; set; }
         public string? ImageURL { get; set; }
 
