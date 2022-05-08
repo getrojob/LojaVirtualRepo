54949993a74b737d4bb4da2950e4d117e2051b4a
getrojob <getrojob@gmai.com>
2022-05-08 18:39:02 -0300

    fix: prop referente a imagem

--- VShop.ProductApi/Models/Product.cs
+++ VShop.ProductApi/Models/Product.cs
@@ -7,7 +7,7 @@ public class Product
     public decimal Price { get; set; }
     public string? Description { get; set; }
     public long Stock { get; set; }
-    public string? ImageUrl { get; set; }
+    public string? ImageURL { get; set; }
 
     public Category? Category { get; set; }
     public int? CategoryId { get; set; }
