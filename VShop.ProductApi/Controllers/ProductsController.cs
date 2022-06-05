749934f9669df21136b1b992f81a62cd91709548
getrojob <getrojob@gmai.com>
2022-06-05 20:28:10 -0300

    add Delete

--- VShop.ProductApi/Controllers/ProductsController.cs
+++ VShop.ProductApi/Controllers/ProductsController.cs
@@ -43,16 +43,12 @@ namespace VShop.ProductApi.Controllers
             return new CreatedAtRouteResult("GetProduct", new { id = productDto.Id }, productDto);
         }
 
-        [HttpPut("{id:int}")]
-        public async Task<ActionResult> Put(int id, [FromBody] ProductDTO productDto)
+        [HttpPut()]
+        public async Task<ActionResult> Put([FromBody] ProductDTO productDto)
         {
-            if (id != productDto.Id)
-                return BadRequest();
 
             if (productDto == null)
-            {
                 return BadRequest();
-            }
 
             await _productService.UpdateProduct(productDto);
 
